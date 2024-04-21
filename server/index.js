1cc4fd43e235cccb479e5596373e0a553bddac9e
Ahmed ImadEldin El-mahady <85259154+ahmedimadeldin@users.noreply.github.com>
2024-04-21 14:42:14 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,13 +13,15 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
-
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://dalle-six-orpin.vercel.app");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
  })
+
+app.use(cors());
+
+
 app.use(express.json({limit: "50mb"}));
 
 // create middleware for the api endpoints :
