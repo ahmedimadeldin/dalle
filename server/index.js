f31267cb3625ae7feb7ae0913bea3072d972b118
Ahmed ImadEldin El-mahady <85259154+ahmedimadeldin@users.noreply.github.com>
2024-04-21 14:33:00 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,16 +12,14 @@ import dalleRoutes from "./routes/dalleRoutes.js";
 dotenv.config();
 
 const app = express();
-const corsOptions = {
-   origin: '*',//(https://your-client-app.com)
-   };
-app.use(cors(corsOptions));
 
-// app.use(function (req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-//  })
+app.use(cors());
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://dalle-six-orpin.vercel.app");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+ })
 app.use(express.json({limit: "50mb"}));
 
 // create middleware for the api endpoints :
